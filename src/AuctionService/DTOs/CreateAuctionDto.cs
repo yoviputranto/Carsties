ef7a111d586294ca07ffb223e8847c7e0dc1a4da
yoviputranto <yoviputrantoo@gmail.com>
2024-04-09 23:06:07 +0700

    Section 2 - Simple Crud Auction

---
+++ src/AuctionService/DTOs/CreateAuctionDto.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionService.DTOs
+{
+    public class CreateAuctionDto
+    {
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Color { get; set; }
+        [Required]
+        public int Mileage { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public int ReservePrice { get; set; }
+        [Required]
+        public DateTime AuctionEnd { get; set; }
+    }
+}
